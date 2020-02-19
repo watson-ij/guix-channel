b112f47d3a1506e23c2123ac497762785caa97d6
Ian J. Watson <ian.james.watson@cern.ch>
2020-02-19 12:52:42 +0900

    add pythia to delphes

--- physics/delphes.scm
+++ physics/delphes.scm
@@ -46,6 +46,15 @@
    (arguments `(#:phases
 		(modify-phases
 		 %standard-phases
+		 (add-after 'unpack 'patch-pythia
+			    (lambda*
+			     (#:key inputs outputs #:allow-other-keys)
+			     (let ((pythia (assoc-ref inputs "pythia")))
+			       (substitute* "CMakeLists.txt"
+					    (("find_package\\(Pythia8\\)")
+					     (string-append "set(PYTHIA8_FOUND 1)
+set(PYTHIA8_INCLUDE_DIRS \"" pythia "/include/Pythia8\")
+set(PYTHIA8_LIBRARIES \"" pythia "/lib/libpythia8.a\")"))))))
 		 (delete 'check))))
    (synopsis "A framework for fast simulation of a generic collider experiment")
    (description " Delphes is a C++ framework, performing a fast multipurpose detector response simulation. The simulation includes a tracking system, embedded into a magnetic field, calorimeters and a muon system. The framework is interfaced to standard file formats (e.g. Les Houches Event File or HepMC) and outputs observables such as isolated leptons, missing transverse energy and collection of jets which can be used for dedicated analyses. The simulation of the detector response takes into account the effect of magnetic field, the granularity of the calorimeters and sub-detector resolutions. Visualisation of the final state particles is also built-in using the corresponding ROOT library.")
