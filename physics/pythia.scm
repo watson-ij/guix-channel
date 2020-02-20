3542064c86c84921e860aff79400e30a85d9b4a6
Ian J. Watson <ian.james.watson@cern.ch>
2020-02-20 17:06:43 +0900

    Compile pythia with zlib, needed by mg5 since it outputs gzip'd lhes

--- physics/pythia.scm
+++ physics/pythia.scm
@@ -3,10 +3,11 @@
   #:use-module (guix packages)
   #:use-module (guix download)
   #:use-module (guix build-system gnu)
-  #:use-module (guix licenses)
+  #:use-module ((guix licenses) #:prefix licenses:)
   #:use-module (gnu packages base)
   #:use-module (gnu packages bash)
   #:use-module (gnu packages commencement)
+  #:use-module (gnu packages compression)
   #:use-module (gnu packages rsync))
 
 (define-public pythia
@@ -26,9 +27,11 @@
    (native-inputs `(("rsync" ,rsync)))
    (inputs `(("cc" ,gcc-toolchain)
 	     ("bash" ,bash)
+	     ("zlib" ,zlib)
 	     ("hepmc" ,hepmc)
 	     ))
-   (arguments `(#:configure-flags (list (string-append "--with-hepmc2=" (assoc-ref %build-inputs "hepmc")))
+   (arguments `(#:configure-flags (list (string-append "--with-hepmc2=" (assoc-ref %build-inputs "hepmc"))
+					(string-append "--with-gzip=" (assoc-ref %build-inputs "zlib")))
 		#:phases
 		(modify-phases
 		 %standard-phases
@@ -36,5 +39,5 @@
    (synopsis "A framework for fast simulation of a generic collider experiment")
    (description " Delphes is a C++ framework, performing a fast multipurpose detector response simulation. The simulation includes a tracking system, embedded into a magnetic field, calorimeters and a muon system. The framework is interfaced to standard file formats (e.g. Les Houches Event File or HepMC) and outputs observables such as isolated leptons, missing transverse energy and collection of jets which can be used for dedicated analyses. The simulation of the detector response takes into account the effect of magnetic field, the granularity of the calorimeters and sub-detector resolutions. Visualisation of the final state particles is also built-in using the corresponding ROOT library.")
    (home-page "https://cp3.irmp.ucl.ac.be/projects/pythia")
-   (license cc-by-sa4.0)
+   (license licenses:cc-by-sa4.0)
    ))
