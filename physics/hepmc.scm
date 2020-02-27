493dc3fc8399dc43f26eebfa8f0eea0c722ff140
Ian J. Watson <ian.james.watson@cern.ch>
2020-02-27 18:07:36 +0900

    Add HepMC3

--- physics/hepmc.scm
+++ physics/hepmc.scm
@@ -40,3 +40,37 @@
    (description "The HepMC package is an object oriented, C++ event record for High Energy Physics Monte Carlo generators and simulation.")
    (home-page "http://hepmc.web.cern.ch/hepmc/")
    (license fsdg-compatible)))
+
+(define-public hepmc-3
+  (package
+   (name "hepmc")
+   (version "3.2.0")
+   (source
+    (origin
+     (method url-fetch)
+     (uri
+      (string-append
+       "http://hepmc.web.cern.ch/hepmc/releases/HepMC3-"
+       version
+       ".tar.gz"))
+     (sha256
+      (base32
+       "1z491x3blqs0a2jxmhzhmh4kqdw3ddcbvw69gidg4w6icdvkhcpi"))))
+   (build-system cmake-build-system)
+   (native-inputs `(("rsync" ,rsync)))
+   (inputs `(("cc" ,gcc-toolchain)
+	     ("bash" ,bash)
+	     ))
+   (arguments
+    `(#:configure-flags
+      '("-DHEPMC3_BUILD_EXAMPLES=OFF"
+	"-DHEPMC3_ENABLE_ROOTIO=OFF"
+	"-DHEPMC3_ENABLE_PYTHON=OFF")
+      #:phases
+      (modify-phases
+       %standard-phases
+       (delete 'check))))
+   (synopsis "HepMC event record library")
+   (description "The HepMC package is an object oriented, C++ event record for High Energy Physics Monte Carlo generators and simulation.")
+   (home-page "http://hepmc.web.cern.ch/hepmc/")
+   (license fsdg-compatible)))
